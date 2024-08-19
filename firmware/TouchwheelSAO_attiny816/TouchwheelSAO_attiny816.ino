43f2e4905f33d4a0adc8748c643f4e99e89d9547
Tod Kurt <todbot@users.noreply.github.com>
2024-08-19 14:58:04 -0700

    add neopixels

--- firmware/TouchwheelSAO_attiny816/TouchwheelSAO_attiny816.ino
+++ firmware/TouchwheelSAO_attiny816/TouchwheelSAO_attiny816.ino
@@ -4,8 +4,12 @@
  * 
  * - Designed for ATtiny816
  * - Uses "megaTinyCore" Arduino core: https://github.com/SpenceKonde/megaTinyCore/
- * - Program with "Uplaod with Programmer" with a USB-Serial dongle 
- *    configured as UPDI programmer ("Tools" / "Programer" / "SerialUPDI - SLOW 57600 baud")
+ * - Configuration in Tools:
+ *   - Defaults
+ *   - Clock: 10 MHz internal
+ *   - printf: minimal
+ *   - Programer: "SerialUPDI - SLOW 57600 baud"
+ * - Program with "Upload with Programmer" with a USB-Serial dongle 
  #    as described here: 
  *    https://learn.adafruit.com/adafruit-attiny817-seesaw/advanced-reprogramming-with-updi
  * 
@@ -13,14 +17,19 @@
 
 #include <Wire.h>
 #include "TouchyTouch.h"
-
-const int touch_pins[] = {0, 1, 2, };
-const int touch_count = sizeof(touch_pins) / sizeof(int);
-TouchyTouch touches[touch_count];
+#include <tinyNeoPixel_Static.h>
+// #include "Adafruit_seesawPeripheral_tinyneopixel.h"
+// using seesaw's neopixel driver instead of megatinycore's tinyneopixel 
+//  because it saves 500 bytes of flash
+// but seems to glitch out, so going back to tinyNeoPixel_Static for now
 
 #define MY_I2C_ADDR 0x54
-#define LED_PIN 16  
+// note these pin numbers are the megatinycore numbers: 
+// https://github.com/SpenceKonde/megaTinyCore/blob/master/megaavr/variants/txy6/pins_arduino.h
+#define LED_STATUS_PIN  5 // PB4, pin 5 on seesaw board
+#define NEOPIXEL_PIN 12  // PC2, pin 12 on seesaw board
 #define MySerial Serial
+#define NUM_LEDS 3
 
 enum Register { 
   REG_POSITION = 0, 
@@ -31,7 +40,7 @@ enum Register {
   REG_RAW1H    = 5,
   REG_RAW2L    = 6,
   REG_RAW2H    = 7,
-  REG_LED      = 8,
+  REG_STATUS_LED = 8,
   REG_THRESH0L = 9,
   REG_THRESH0H = 10,
   REG_THRESH1L = 11,
@@ -45,8 +54,36 @@ enum Register {
   REG_NONE     = 255,
 };
 
+const int touch_pins[] = {0, 1, 2, };
+const int touch_count = sizeof(touch_pins) / sizeof(int);
+TouchyTouch touches[touch_count];
+
+volatile uint8_t neopixel_buf[NUM_LEDS*3]; // 3 bytes per LED
+tinyNeoPixel leds = tinyNeoPixel(NUM_LEDS, NEOPIXEL_PIN, NEO_GRB, (uint8_t*) neopixel_buf);
+
 uint8_t regs[16]; // the registers to send/receive via i2c
 uint8_t curr_reg = REG_POSITION; 
+
+#define pixel_set(n,r,g,b) leds.setPixelColor(n, leds.Color(r,g,b))
+#define pixel_fill(r,g,b) leds.fill(leds.Color(r,g,b))
+#define pixel_show() leds.show()
+
+// void pixel_set(uint8_t n, uint8_t r, uint8_t g, uint8_t b) {
+//   neopixel_buf[n*3+0] = g; // G
+//   neopixel_buf[n*3+1] = r; // R 
+//   neopixel_buf[n*3+2] = b; // B
+// }
+
+// void pixel_fill(uint8_t r, uint8_t g, uint8_t b) { 
+//   for(int i=0; i<NUM_LEDS; i++) { 
+//     pixel_set(i, r,g,b);
+//   }
+// }
+
+// void pixel_show() {
+//     tinyNeoPixel_show(NEOPIXEL_PIN, NUM_LEDS*3, (uint8_t *)neopixel_buf);
+// }
+
 
 float wheel_pos(float offset=0) { 
   // compute raw percentages
@@ -88,58 +125,6 @@ float wheel_pos(float offset=0) {
 
 }
 
-void setup() {
-  MySerial.begin(115200);
-  MySerial.println("\r\nHello I'm an I2C device");
-
-  pinMode(LED_PIN, OUTPUT);
-
- // Touch buttons
-  for (int i = 0; i < touch_count; i++) {
-    touches[i].begin( touch_pins[i] );
-    touches[i].threshold = touches[i].raw_value * 1.1;
-  }
-  
-  Wire.onReceive(receiveDataWire);
-  Wire.onRequest(transmitDataWire);
-  Wire.begin(MY_I2C_ADDR);
-}
-
-uint32_t last_debug_time;
-
-void loop() {
-  for ( int i = 0; i < touch_count; i++) {
-    touches[i].update(); 
-  }
-
-  // fixme add filtering here
-
-  float fpos = wheel_pos();
-  uint8_t pos = 0;
-  if(fpos > -1) { 
-    pos = fpos * 255;
-  }
-
-  // set the registers
-  regs[REG_POSITION] = pos;  // FIXME
-  regs[REG_TOUCHES] = touches[0].touched() << 2 | touches[1].touched() << 1 | touches[2].touched();
-  regs[REG_RAW0L] = touches[0].raw_value;  // high and low byte
-  regs[REG_RAW0H] = touches[0].raw_value>>8;  // high and low byte
-  regs[REG_RAW1L] = touches[1].raw_value;  // high and low byte
-  regs[REG_RAW1H] = touches[1].raw_value>>8;  // high and low byte
-  regs[REG_RAW2L] = touches[2].raw_value;  // high and low byte
-  regs[REG_RAW2H] = touches[2].raw_value>>8;  // high and low byte
-
-  // act on output registers
-  digitalWrite(LED_PIN, regs[REG_LED] > 0 ? HIGH : LOW);
-
-  // debug
-  if( millis() - last_debug_time > 300 ) { 
-    last_debug_time = millis();
-    MySerial.printf("pos: %d\r\n", pos);
-  }
-}
-
 void receiveDataWire(int16_t numBytes) {      // the Wire API tells us how many bytes
   curr_reg = Wire.read();                  // first byte is address to read from / write to
   MySerial.printf("recv: reg:%02x ", curr_reg);
@@ -159,5 +144,86 @@ void transmitDataWire() {
   //MySerial.printf("send: %02x\r\n", c);
   Wire.write(c);
   curr_reg++;  // FIXME: test this
+}
+
+
+void setup() {
+  MySerial.begin(115200);
+  MySerial.println("\r\nHello I'm an I2C device");
+
+  pinMode(LED_STATUS_PIN, OUTPUT);
+  
+  pinMode(NEOPIXEL_PIN, OUTPUT);
+  // leds.setPixelColor(0, leds.Color(0, 150, 0)); // Moderately bright green color.
+  // leds.show(); // This sends the updated pixel color to the hardware.  
+  pixel_set(0, 0x33, 0x00, 0x33);
+  pixel_show();
+      
+ // Touch buttons
+  for (int i = 0; i < touch_count; i++) {
+    touches[i].begin( touch_pins[i] );
+    touches[i].threshold = touches[i].raw_value * 1.1;
+  }
+  
+  Wire.onReceive(receiveDataWire);
+  Wire.onRequest(transmitDataWire);
+  Wire.begin(MY_I2C_ADDR);
+}
+
+uint32_t last_debug_time;
+uint32_t last_led_time;
+uint8_t pos = 0;
+
+void loop() {
+  for ( int i = 0; i < touch_count; i++) {
+    touches[i].update(); 
+  }
+
+  // simple iir filtering 
+  float fpos = wheel_pos();
+  const float filt = 0.05;
+  if(fpos > -1) { 
+    fpos = (fpos * 255);
+    pos = filt * fpos + (1-filt) * pos;
+  }
+  else { 
+    pos = 0;
+  }
+
+  // set the registers
+  regs[REG_POSITION] = pos;  // FIXME
+  regs[REG_TOUCHES] = touches[0].touched() << 2 | touches[1].touched() << 1 | touches[2].touched();
+  regs[REG_RAW0L] = touches[0].raw_value;  // high and low byte
+  regs[REG_RAW0H] = touches[0].raw_value>>8;  // high and low byte
+  regs[REG_RAW1L] = touches[1].raw_value;  // high and low byte
+  regs[REG_RAW1H] = touches[1].raw_value>>8;  // high and low byte
+  regs[REG_RAW2L] = touches[2].raw_value;  // high and low byte
+  regs[REG_RAW2H] = touches[2].raw_value>>8;  // high and low byte
+
+  // act on output registers
+  digitalWrite(LED_STATUS_PIN, regs[REG_STATUS_LED] > 0 ? HIGH : LOW);
+
+  uint32_t now = millis();
+  if( now - last_led_time > 1 ) { // only update neopixels every 1 ms
+    last_led_time = now;
+    pixel_fill(0,0,0);
+    if( pos < 85 ) {
+      pixel_set(0, pos, 0, 0);
+    }
+    else if( pos < 170 ) {
+      pixel_set(1, pos-85, 0, 0);
+    }
+    else {
+      pixel_set(2, pos-170, 0, 0);
+    }
+
+    pixel_show();
+  }
+
+  // debug
+  if( now - last_debug_time > 50 ) { 
+    last_debug_time = now;
+    MySerial.printf("pos: %d\r\n", pos);
+  }
 }
 
---
+++ firmware/tester/code.py
@@ -0,0 +1,55 @@
+# touchwheelsao_tester.py -- simple I2C host for TouchWheelSAO
+# 5 Aug 2024 - @todbot / Tod Kurt
+#
+
+import time, board, busio
+from adafruit_bus_device import i2c_device
+
+i2c_addr = 0x54
+i2c = busio.I2C(board.GP17, board.GP16)
+i2cdev = i2c_device.I2CDevice(i2c, i2c_addr)
+
+def i2c_scan(i2c):
+    while not i2c.try_lock(): pass
+    print("I2C addresses found:",
+          [hex(device_address) for device_address in i2c.scan()])
+    i2c.unlock()
+
+def i2c_write_register(_i2cdev, reg_addr, reg_val):
+    """Write 8 bit value to register at address."""
+    _buf = bytearray(2)
+    _buf[0] = reg_addr
+    _buf[1] = reg_val
+    with _i2cdev as myi2cdev:
+        myi2cdev.write(_buf)
+
+def i2c_read_register_bytes(_i2cdev, reg_addr, num_bytes):
+    with _i2cdev as myi2cdev:
+        myi2cdev.write(bytes([reg_addr]))
+        result = bytearray(num_bytes)
+        myi2cdev.readinto(result)
+    return result
+
+
+led_state = 0
+
+while True:
+    i2c_scan(i2c)
+    try:
+        print("%.2f write reg:" % time.monotonic())
+        i2c_write_register(i2cdev, 8, led_state)
+        led_state = not led_state
+        print("reading...:")
+        r = i2c_read_register_bytes(i2cdev, 0, 1)
+        print("  pos:", r, r[0])
+        r = i2c_read_register_bytes(i2cdev, 1, 1)
+        print("  touches:", r, f'{r[0]:03b}')
+        r = i2c_read_register_bytes(i2cdev, 2, 1) # RAW0L
+        print("  raw0L:", r, r[0])
+        r = i2c_read_register_bytes(i2cdev, 3, 1) # RAW0H
+        print("  raw0H:", r, r[0])
+        
+    except OSError as e:
+        print(e)
+    time.sleep(1)
+    
